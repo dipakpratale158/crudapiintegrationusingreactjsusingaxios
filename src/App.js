8772e20099669c0490a528c7ca0a19717701f6b8
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-04-14 19:45:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CRUD from "./pages/Crud";
+import styles from "./styles.css";
+function App() {
+  return (
+    <div className="App">
+      <CRUD />
+    </div>
+  );
+}
+
+export default App;
